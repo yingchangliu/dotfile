3c9117b43d30cd75e87cac755d8ef535d5f5b3d1
kelen <58721349+yingchangliu@users.noreply.github.com>
2022-05-14 17:31:10 +0800

    update .gitignore

--- config/autostart.sh
+++ config/autostart.sh
@@ -14,7 +14,7 @@ killall swhks
 swhks &
 pkexec swhkd # -c $HOME/.config/swhkd/swhkdrc &
 sh $HOME/.config/bin/pkill_bc &
-swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
+#swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
 
 run mako
 swayidle -w timeout 300 ~/.config/bin/wayland_session_lock &
--- config/bin/wallpaper.sh
+++ config/bin/wallpaper.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 swaybg_instances=$(ps axh | grep swaybg | grep -v grep | awk '{print $1}')
-
+wallpath=~/dotfile/config/background/
 if [[ -f /tmp/currentwall ]];then
-	image=$( find /usr/share/backgrounds/ -type f | grep -v $(cat /tmp/currentwall) | shuf | head -n 1)
+    image=$( find ${wallpath} -type f | grep -v $(cat /tmp/currentwall) | shuf | head -n 1)
 else
-	image=$(find /usr/share/backgrounds/ -type f | shuf | head -n 1)
+    image=$(find ${wallpath} -type f | shuf | head -n 1)
 fi
-
+#if [[ -f /tmp/currentwall ]];then
+#	image=$( find /usr/share/backgrounds/ -type f | grep -v $(cat /tmp/currentwall) | shuf | head -n 1)
+#else
+#	image=$(find /usr/share/backgrounds/ -type f | shuf | head -n 1)
+#fi
+echo $image
 echo $image > /tmp/currentwall
 setsid -f swaybg -i $image
 sleep 1 # for a smooth transition with no flicker
