9b9a6b022b5a9ce71367089e363ec019301e5856
kelen <58721349+yingchangliu@users.noreply.github.com>
2022-05-19 13:03:05 +0800

    0519

--- config/autostart.sh
+++ config/autostart.sh
@@ -7,23 +7,24 @@ function run {
   fi
 }
 
-sh $HOME/.config/bin/pkill_bc &
+sh $HOME/.config/bin/pkill_bc & 2>/dev/null
 #killall swaybg
 #killall waybar
-run waybar &
-killall swhks
-swhks &
-pkexec swhkd # -c $HOME/.config/swhkd/swhkdrc &
+run waybar & 2>/dev/null
 #swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
 
-run mako
-swayidle -w timeout 300 ~/.config/bin/wayland_session_lock   before-sleep 'swaylock -f -c 000000'&
+run mako & 2>/dev/null
+
+swayidle -w timeout 300 ~/.config/bin/wayland_session_lock   before-sleep 'swaylock -f -c 000000'& 2>/dev/null
 
 #run swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
 #run nm-applet  # network-manager-applet
 #run mpd
 #run xfce4-power-manager --no-daemon
 #run blueman-applet
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & 2>/dev/null
 #run fcitx5 -d
 #run thunar --daemon
+killall swhks 2>/dev/null
+swhks & 2>/dev/null
+pkexec swhkd  2>/dev/null  # -c $HOME/.config/swhkd/swhkdrc &
