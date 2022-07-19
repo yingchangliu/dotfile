c7a925c5401d7d68562386e4999a48e4b5906546
yingchangliu <58721349+yingchangliu@users.noreply.github.com>
2022-07-19 18:46:29 +0800

    apply patches to dmenu

--- config/autostart.sh
+++ config/autostart.sh
@@ -7,30 +7,30 @@ function run {
   fi
 }
 
-sh $HOME/.config/bin/pkill_bc & 2>/dev/null
+sh $HOME/.config/bin/pkill_bc
 #killall swaybg
 #killall waybar
-#run waybar & 2>/dev/null
+#run waybar
 #swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
 
-#run mako list & 2>/dev/null
+#run mako list
 
-#swayidle -w timeout 3000 ~/.config/bin/wayland_session_lock   before-sleep 'swaylock -f -c 000000'& 2>/dev/null
-#run xfce4-power-manager & 2>/dev/null
-run dunst & 2>/dev/null
-run dwmblocks $ 2>/dev/null
-run picom & 2>/dev/null
-
+#swayidle -w timeout 3000 ~/.config/bin/wayland_session_lock   before-sleep 'swaylock -f -c 000000'
+#run xfce4-power-manager
+run dunst
+run dwmblocks
+run picom
+run conky -c ~/.config/conky/conkyrc
 #run swaybg -i $(find $HOME/.config/background/. -type f | shuf -n 1) -m fill &
 #run nm-applet  # network-manager-applet
 #run mpd
 #run xfce4-power-manager --no-daemon
 #run blueman-applet
-run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & 2>/dev/null
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run fcitx5 -d
-run udiskie & 2>/dev/null
+run udiskie
 #run thunar --daemon
 
-killall swhks 2>/dev/null
-swhks & 2>/dev/null
-pkexec swhkd  2>/dev/null  # -c $HOME/.config/swhkd/swhkdrc &
+killall swhks
+swhks
+pkexec swhkd  # -c $HOME/.config/swhkd/swhkdrc &
